b3d467e0b89e05f8f1319cea2314e7e2ee739f12
Viktoriia Zakhvatkina <vikapomme@gmail.com>
2022-03-16 00:41:37 +0300

    add rest timeout

--- src/main/java/vic/ds/msnumbers/Controller.java
+++ src/main/java/vic/ds/msnumbers/Controller.java
@@ -1,8 +1,11 @@
 package vic.ds.msnumbers;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class Controller {
@@ -14,7 +17,12 @@ public class Controller {
     }
 
     @PostMapping("{num}")
+    @Transactional(timeoutString = "1")
     public String processNum(@PathVariable Integer num){
-        return numbersService.processNum(num);
+        try {
+            return numbersService.processNum(num);
+        } catch (Exception e){
+            return "Request Timeout";
+        }
     }
 }
--- src/main/java/vic/ds/msnumbers/NumbersRepository.java
+++ src/main/java/vic/ds/msnumbers/NumbersRepository.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,28 +21,31 @@ public class NumbersRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public String checkInDb(Integer num){
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE, timeout = 1)
+    public String checkInDb(Integer num) {
         String ans = "Invalid number was given.";
-        if (num<0) return ans;
+        if (num < 0) return ans;
         String query = String.format("select id from numbers where id = %s or id = %s", num, num + 1);
         List<String> result = jdbcTemplate.queryForList(query, String.class);
-        if (result.isEmpty()){
-            jdbcTemplate.update(String.format("insert into numbers (id) values (%s)", num));
-            ans = String.valueOf(num+1);
-        }
-        else if (result.size()==2){
+        if (result.isEmpty()) {
+            try {
+                jdbcTemplate.update(String.format("insert into numbers (id) values (%s)", num));
+                ans = String.valueOf(num + 1);
+            } catch (Exception e) {
+                ans = String.format("Number %s is already in db.", num);
+                LOGGER.error(String.format("Number %s is already in db.", num));
+            }
+        } else if (result.size() == 2) {
             ans = String.format("Both %s and following number %s are already in db.", num, num + 1);
             LOGGER.error(String.format("Both %s and following number %s are already in db.", num, num + 1));
-        }
-        else if (result.get(0).equals(num.toString())){
-            ans = String.format("Number %s is already in db.", num, result.get(0));
+        } else if (result.get(0).equals(num.toString())) {
+            ans = String.format("Number %s is already in db.", num);
             LOGGER.error(String.format("Number %s is already in db.", num));
-        }
-        else {
+        } else {
             ans = String.format("Following number %s is already in db.", num + 1);
             LOGGER.error(String.format("Following number %s is already in db.", num + 1));
         }
         return ans;
     }
+
 }
